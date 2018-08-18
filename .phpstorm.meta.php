a6eb56485428a7b46c73e304bb76b701fd0ab22c
guitarbien <guitarbien@gmail.com>
2018-08-18 22:14:03 +0800

    composer require firebase/php-jwt

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -3,7 +3,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2018-08-18 03:59:03.
+    * Generated on 2018-08-18 14:13:50.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
--- _ide_helper.php
+++ _ide_helper.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.6.33 on 2018-08-18 05:00:45.
+ * Generated for Laravel 5.6.33 on 2018-08-18 14:13:46.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
